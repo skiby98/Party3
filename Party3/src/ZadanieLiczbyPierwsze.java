605ea471d4891f521678e8cfdc6b4f6e5da9d0f6
skiby98 <skibickimaksymilian@gmail.com>
2023-09-26 22:58:55 +0200

    Dodanie wyświetlenia tekstu wskazującego by użytkownik zdefiniował liczbę szufladek na dane a nastepnie podal wartosci dla poszczegolnych szufladek

--- Party3/src/ZadanieLiczbyPierwsze.java
+++ Party3/src/ZadanieLiczbyPierwsze.java
@@ -4,14 +4,16 @@ public class ZadanieLiczbyPierwsze {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Podaj liczbę szufladek: ");
         int n = scanner.nextInt();
 
         int[] tab = new int[n];
 
+        System.out.println("Podaj wartości dla kazdej szuflladki: ");
         for (int i=0; i < n; i++){
             tab[i] = scanner.nextInt();
         }
-
+        
         for (int i=0; i < n; i++) {
             if (tab[i]>10) {
                 System.out.println("TAK");
